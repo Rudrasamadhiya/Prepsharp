904d1aeb1660c99f0d5a2f760b1238fb2d48d8c8
Rudrasamadhiya <Samadhiyarudra123@gmail.com>
2025-06-11 18:08:21 +0530

    ok

---
+++ pro/config.js
@@ -0,0 +1,4 @@
+// API Configuration
+const config = {
+  OPENAI_API_KEY: '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'
+};
